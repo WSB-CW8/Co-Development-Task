25e275b07877857a3d5a1830b3544177398f8e2b
Jakub Rastawicki <j.rastawicki@poczta.fm>
2024-10-20 14:36:21 +0200

    Setuped Repo

---
+++ api/src/main/java/org/example/HelloController.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
+}
