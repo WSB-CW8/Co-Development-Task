3cf9bdc12afb13581c5524c2f30eabd131825d20
Maksymilian Grupiński <62545404+Kinggred@users.noreply.github.com>
2024-11-06 22:57:49 +0100

    V0.2 (#42)
    * Initial setup
    
    * feat: initial files
    
    * Test commita
    
    Signed-off-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    
    * Przepis na bigos
    
    Signed-off-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    
    * Chujowy ten przepis
    
    Signed-off-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    
    * Przepraszam Panie Profesorze, Main nie był zabezpieczony
    
    Signed-off-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    
    * feat: install vite
    
    * Pain of spring setup is done
    
    * Setuped Repo
    
    * Fixed stuff
    
    * Testing endpoint with "Hello World"
    
    * ignored folder .idea/
    
    * removed folder .idea
    
    ---------
    
    Co-authored-by: Maksymilian Grupiński <maks.grupinski@solve.md>
    
    ---------
    
    Signed-off-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    Signed-off-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    Co-authored-by: Artur Kuciński <artuyees@gmail.com>
    Co-authored-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    Co-authored-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    Co-authored-by: Artur Kuciński <artur.k@masterborn.com>
    Co-authored-by: Juan Pablo Secundo <JP2@GMD.pl>
    Co-authored-by: AstralJacob <103335943+AstralJacob@users.noreply.github.com>
    Co-authored-by: Maksymilian Grupiński <maks.grupinski@solve.md>
    
    * Dodanie wyświetlania mapy przy użyciu biblioteki leaflet
    
    * Implemented models of received data (#37)
    
    * Created DataRetrievalService (#38)
    
    * Created DataRetrievalService
    
    * Removed API_KEY from repo
    
    ---------
    
    Co-authored-by: Maksymilian Grupiński <62545404+Kinggred@users.noreply.github.com>
    
    * Stworzyłem dockerfile i docker compose (#40)
    
    Added Docker
    
    * [BE]-5-29-Added-bus-endpoint (#41)
    
    * Fuckup Backup
    
    * It just works (powazny commit)
    
    ---------
    
    Signed-off-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    Signed-off-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    Co-authored-by: Artur Kuciński <artuyees@gmail.com>
    Co-authored-by: Mateusz Kozłowski <58187885+LoQeN00@users.noreply.github.com>
    Co-authored-by: Patryk Łasek <105239767+lasekpatryk@users.noreply.github.com>
    Co-authored-by: Artur Kuciński <artur.k@masterborn.com>
    Co-authored-by: Juan Pablo Secundo <JP2@GMD.pl>
    Co-authored-by: AstralJacob <103335943+AstralJacob@users.noreply.github.com>
    Co-authored-by: Maksymilian Grupiński <maks.grupinski@solve.md>
    Co-authored-by: loqen00 <loqen00@o2.pl>
    Co-authored-by: Rafał Jarnutowski <147340355+Red0n@users.noreply.github.com>
    Co-authored-by: Sebastian Kowalczyk <119742268+skkowalczyk@users.noreply.github.com>
    Co-authored-by: Konrad-161139 <137608141+Konrad-23257@users.noreply.github.com>

---
+++ api/src/main/java/com/c8/api/services/DataRetrievalService.java
@@ -0,0 +1,45 @@
+package com.c8.api.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.c8.api.models.IncomingData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Locale;
+import java.util.Objects;
+
+@Service
+public class DataRetrievalService {
+
+    private final RestTemplate restTemplate;
+    // TODO: move outside
+    final String API_KEY = "&apikey=";
+
+    @Autowired
+    public DataRetrievalService(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    // Not scheduling for now
+    // @Scheduled(fixedRate = 10000)
+    public IncomingData fetchData(){
+        String url = "https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e-927d-4ad3-9500-4ab9e55deb59&type=1" + API_KEY;
+
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            String jsonResponse = Objects.requireNonNull(response.getBody()).toLowerCase(Locale.ROOT);
+
+            // Parse or use the data as needed
+            return mapper.readValue(jsonResponse, IncomingData.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
