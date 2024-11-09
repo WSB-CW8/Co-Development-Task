e032fe12eeb0771fbc322c0a8fa747ab10c4951d
Kamil Dominik <kamil.dominik44@wp.pl>
2024-11-09 22:21:04 +0100

    Dodanie pozostałych zajezdni: - 'Stalowa' - 'Worownicza'

--- api/src/main/java/com/c8/api/globals/DepotCoordinates.java
+++ api/src/main/java/com/c8/api/globals/DepotCoordinates.java
@@ -10,6 +10,8 @@ public class DepotCoordinates {
     public DepotCoordinates(){
         depots.add(new BusDepot("Ostrobramska", 52.235241, 52.232805, 21.113718, 21.118523));
         depots.add(new BusDepot("Kleszczowa", 52.197001, 52.196628, 20.918370, 20.923418));
+        depots.add(new BusDepot("Stalowa", 52.265185, 52.264654, 21.042578, 21.049445));
+        depots.add(new BusDepot("Worownicza", 52.188737, 52.185656, 20.995111, 20.998673));
     }
 
     public List<BusDepot> getDepots() {
