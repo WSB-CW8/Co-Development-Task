6e06efcf5ccc1a7c47a6feb20559de48a9e1485b
Maksymilian Grupiński <maks.grupinski@gmail.com>
2024-11-14 20:27:16 +0100

    Fixed stuf

--- api/src/main/java/com/c8/api/WebConfig.java
+++ api/src/main/java/com/c8/api/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET")
                 .allowedHeaders("*")
                 .allowCredentials(true);
