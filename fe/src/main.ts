70a5f373d8350e92eea30e71ff2d41c5b286f686
Maksymilian Grupiński <maks.grupinski@gmail.com>
2024-11-20 17:47:20 +0100

    Fixed conflict fix as it was broken

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -1,50 +1,32 @@
 import "./style.css";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
 
+import { eventBus } from "./core/eventBus/EventBus";
+import { MapManager } from "./core/managers/MapManager";
+import { UIManager } from "./core/managers/UIManager";
+import { BusManager } from "./core/managers/BusManager";
+
+// Managers
+
+// Main App Initialization
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  
-    <div>
-      <h1>Tutaj coś będzie kiedyś</h1>
-    </div>
-    
+    <h1>Bus Tracker</h1>
     <div id="map"></div>
- 
 `;
 
-const map = L.map("map").setView([52.259788, 21.040546], 13);
+new MapManager(eventBus, "map", [52.259788, 21.040546], 13);
+const busManager = new BusManager(eventBus);
+const uiManager = new UIManager(eventBus);
 
-// Load and display tile layer (OpenStreetMap tiles)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
+const apiUrl = import.meta.env.VITE_API_URL;
 
-type BusData = {
-  lat: number;
-  lon: number;
-  lines: string;
-  vehiclenumber: string;
-  time: string;
-  brigade: string;
-};
+busManager
+    .fetchBuses(apiUrl, uiManager.getFilterSelectValue())
+    .then(() => console.log("Buses fetched and rendered."));
 
-const fetchBusData = async () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return (await response.json()) as { result: BusData[] };
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-  }
-};
-const renderBuses = async () => {
-  const busData = await fetchBusData();
-  busData?.result.forEach((element) => {
-    L.marker([element.lat, element.lon]).addTo(map);
-  });
-};
-
-renderBuses().then(() => console.log("Buses rendered")); //TODO do implementacji w dalszych taskach
+// Initial Data Fetch
+setInterval(() => {
+  busManager
+      .fetchBuses(apiUrl, uiManager.getFilterSelectValue())
+      .then(() => console.log("Buses fetched and rendered."));
+}, 10000);
